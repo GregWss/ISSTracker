37765fa3358ea510dc701a8bdd742f08604e5624
Greg_wss <weiss.gregory48@orange.fr>
2019-01-28 18:19:19 -0500

    Premier commit

---
+++ app/src/main/java/uqac/ca/isstracker/MainActivity.java
@@ -0,0 +1,72 @@
+package uqac.ca.isstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
+//Volley imports
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Bouton qui fait la requête lorsque cliqué
+        Button requete = (Button) findViewById(R.id.requete);
+        requete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final TextView affichageReq = (TextView) findViewById(R.id.res_requete);
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue;
+
+                // Instantiate the cache
+                Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+                // Set up the network to use HttpURLConnection as the HTTP client.
+                Network network = new BasicNetwork(new HurlStack());
+
+                // Instantiate the RequestQueue with the cache and network.
+                queue = new RequestQueue(cache, network);
+
+                // Start the queue
+                queue.start();
+
+                String url ="http://api.open-notify.org/iss-now.json";
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                affichageReq.setText(response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        affichageReq.setText("That didn't work!");
+                    }
+                });
+
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+            }
+        });
+    }
+}
