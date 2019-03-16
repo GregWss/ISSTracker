118d7db0e57e02bc0dc149c2f64edeebec359fde
Greg_wss <weiss.gregory48@orange.fr>
2019-03-16 15:21:25 -0400

    Adding position on map and refreshing

--- app/src/main/java/uqac/ca/isstracker/activities/MapActivity.java
+++ app/src/main/java/uqac/ca/isstracker/activities/MapActivity.java
@@ -1,20 +1,49 @@
 package uqac.ca.isstracker.activities;
 
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
+import android.widget.Toast;
+import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import uqac.ca.isstracker.R;
+import uqac.ca.isstracker.model.ISSNow;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback
 {
+    public static final String TAG = "ACTIVITY - MAP";
+
     private GoogleMap mMap;
+
+    // Data
+    private ISSNow issData;
+    private float ISSLat;
+    private float ISSLng;
+
+    // Refresh
+    private Timer timer;
+    private TimerTask refreshData;
+    private Handler mTimerHandler = new Handler();
+
+    // Request
+    private RequestQueue mRequestQueue;
+    private static final String open_notify_url_iss_now = "http://api.open-notify.org/iss-now";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,7 +55,6 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
 
     /**
      * Manipulates the map once available.
@@ -42,9 +70,72 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback
     {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        requestAndDataTreatment();
+    }
+
+    public void requestAndDataTreatment(){
+        timer = new Timer();
+
+        refreshData = new TimerTask() {
+            public void run() {
+                mTimerHandler.post(new Runnable() {
+                    public void run() {
+                        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, open_notify_url_iss_now,
+                                null, new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    // Getting data
+                                    issData = new ISSNow(response);
+
+                                    // Getting ISSPosition
+                                    ISSLng = issData.getIssLongitude();
+                                    ISSLat = issData.getIssLatitude();
+
+                                    Toast.makeText(getApplicationContext(), "" + issData.getIssLatitude(), Toast.LENGTH_LONG).show();
+
+                                    // Add a marker on the ISS and move the camera
+                                    LatLng ISS = new LatLng(ISSLat, ISSLng);
+                                    mMap.addMarker(new MarkerOptions().position(ISS).title("Marker of the ISS"));
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(ISS));
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError e) {
+                                //TODO
+                            }
+                        });
+
+                        // Access the RequestQueue through your singleton class.
+                        addToRequestQueue(jsonObjectRequest);
+                    }
+                });
+            }
+        };
+
+        timer.schedule(refreshData,1,5000);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req)
+    {
+        // set the default tag if tag is empty
+        req.setTag(TAG);
+
+        getRequestQueue().add(req);
+    }
+
+    public RequestQueue getRequestQueue()
+    {
+        // lazy initialize the request queue, the queue instance will be
+        // created when it is accessed for the first time
+        if (this.mRequestQueue == null)
+        {
+            this.mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
     }
 }
